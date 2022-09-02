f60ce1d514f3c120bca3df96e067f00d14e539ff
Simon Gil <simon@virus.local>
2022-09-01 21:16:46 -0400

    some more updates

--- Python/ump-tracker-v0.2.py
+++ Python/ump-tracker-v0.2.py
@@ -13,9 +13,7 @@ inning = 1
 point_mod = int()
 
 while True:
-    if strikes == 2:
-        options.pop(2)
-    elif outs == 3:
+    if outs == 3:
         print('balls: ' + str(balls))
         print('strikes: ' + str(strikes))
         print('outs: ' + str(outs))
@@ -49,9 +47,24 @@ while True:
         if top_inn == False:
             inning += 1
             continue
-        else:
-            top_inn == False
+        elif top_inn:
+            top_inn = False
             continue
+        elif inning == 9 and top_inn and home_score > away_score:
+            print('balls: ' + str(balls))
+            print('strikes: ' + str(strikes))
+            print('outs: ' + str(outs))
+            print(HOME_TEAM + ': ' + str(home_score))
+            print(AWAY_TEAM + ': ' + str(away_score))
+            print(HOME_TEAM + ' wins!')
+        elif inning == 9 and top_inn == False and away_score > home_score:
+            print('balls: ' + str(balls))
+            print('strikes: ' + str(strikes))
+            print('outs: ' + str(outs))
+            print(HOME_TEAM + ': ' + str(home_score))
+            print(AWAY_TEAM + ': ' + str(away_score))
+            print(AWAY_TEAM + ' wins!')
+
     elif strikes == 3:
         outs += 1
         strikes = 0
