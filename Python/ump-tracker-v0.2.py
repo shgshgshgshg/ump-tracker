b605ca1e2e1f91202fb4e6c87c36e1738ecb654a
Simon Gil <simon@virus.local>
2022-09-02 22:35:44 -0400

    it actually works now :D

--- Python/ump-tracker-v0.2.py
+++ Python/ump-tracker-v0.2.py
@@ -11,37 +11,18 @@ AWAY_TEAM = 'NYY'
 top_inn = True
 inning = 1
 point_mod = int()
-INNINGS_TOTAL = input('How many innings will be played? ')
+INNINGS_TOTAL = int(input('How many innings will be played? '))
+def scoreboard():
+    print('balls: ' + str(balls))
+    print('strikes: ' + str(strikes))
+    print('outs: ' + str(outs))
+    print(HOME_TEAM + ': ' + str(home_score))
+    print(AWAY_TEAM + ': ' + str(away_score))
+    print('Inning: ' + str(inning))
+    print('------------------------------------------')
 
-while True:
-    if outs == 3:
-        print('balls: ' + str(balls))
-        print('strikes: ' + str(strikes))
-        print('outs: ' + str(outs))
-        print(HOME_TEAM + ': ' + str(home_score))
-        print(AWAY_TEAM + ': ' + str(away_score))
-        print('')
-        inning += 1
-        continue
-    elif top_inn:
-        top_inn = False
-        continue
-    elif inning == INNINGS_TOTAL and top_inn and home_score > away_score:
-        print('balls: ' + str(balls))
-        print('strikes: ' + str(strikes))
-        print('outs: ' + str(outs))
-        print(HOME_TEAM + ': ' + str(home_score))
-        print(AWAY_TEAM + ': ' + str(away_score))
-        print('Inning: ' + str(inning))
-        print(HOME_TEAM + ' wins!')
-    elif inning == INNINGS_TOTAL and top_inn == False and away_score > home_score:
-        print('balls: ' + str(balls))
-        print('strikes: ' + str(strikes))
-        print('outs: ' + str(outs))
-        print(HOME_TEAM + ': ' + str(home_score))
-        print(AWAY_TEAM + ': ' + str(away_score))
-        print('Inning: ' + str(inning))
-        print(AWAY_TEAM + ' wins!')
+while True:    
+    scoreboard()
 
     options = ['[s] strike', '[b] ball', '[f] foul', '[p] points modifier', '[x] modify other']
     menu = TerminalMenu(options)
@@ -62,33 +43,47 @@ while True:
             away_score = int(away_score) + int(point_mod)
         else:
             home_score = int(home_score) + int(point_mod)
-        point_mod = 0
     elif idx == 4:
         print("To determine what to change, write 's' for strike,")
         print("'b' for balls, and 'o' for outs. Then write the number.")
         modifier = input('')
         modified = modifier.split()
+        print(modified)
         if modified[0] == 'b':
             balls += int(modified[1])
             print('ball')
         elif modified[0] == 's':
             strikes += int(modified[1])
+            strikes = max(0, strikes)
             print('strike')
         elif modified[0] == 'o':
             outs += int(modified[1])
             print('out')
-    if strikes == 3:
+
+    if strikes >= 3:
         outs += 1
         strikes = 0
         balls = 0
-    elif balls == 4:
+    elif balls >= 4:
         strikes = 0
         balls = 0
 
-    print('balls: ' + str(balls))
-    print('strikes: ' + str(strikes))
-    print('outs: ' + str(outs))
-    print(HOME_TEAM + ': ' + str(home_score))
-    print(AWAY_TEAM + ': ' + str(away_score))
-    print('Inning: ' + str(inning))
-    print('------------------------------------------')
+    if outs >= 3:
+        outs = 0
+        strikes = 0
+        balls = 0
+        top_inn = not top_inn
+        if top_inn:
+            inning += 1
+
+    if inning >= INNINGS_TOTAL and top_inn and home_score > away_score:
+        scoreboard()
+        print(HOME_TEAM + ' wins!')
+        break
+
+    if inning >= INNINGS_TOTAL and top_inn == False and away_score > home_score:
+        scoreboard()
+        print(AWAY_TEAM + ' wins!')
+        break
+
+
